bc74ab1bd178f690a8ca53983ad6b367db8df2db
giulia-ciampa <giulia.ciampa7@gmail.com>
2026-07-20 14:41:55 +0200

    Initial commit

---
+++ src/main/java/team6/BW_5/Bw5Application.java
@@ -0,0 +1,13 @@
+package team6.BW_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bw5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bw5Application.class, args);
+	}
+
+}
---
+++ src/test/java/team6/BW_5/Bw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package team6.BW_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bw5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
