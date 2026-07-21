f53f3776ad993c6c6d383a0d19074304519b80fe
itsemanuela <notemanu152@gmail.com>
2026-07-21 11:48:56 +0200

    Utente Response e Request DTO aggiunti

--- src/main/java/team6/BW_5/services/UtenteService.java
+++ src/main/java/team6/BW_5/services/UtenteService.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import team6.BW_5.entities.Utente;
+import team6.BW_5.exceptions.NotFoundException;
 import team6.BW_5.repositories.UtenteRepository;
 
 import java.util.List;
@@ -53,6 +54,12 @@ public class UtenteService {
         utenteEsistente.setPassword(utenteModificato.getPassword());
 
         return utenteRepository.save(utenteEsistente);
+    }
+    //delete per utente tramite id
+  public void eliminaUtente(UUID id) {
+        Utente utenteDaEliminare = utenteRepository.findById(id).orElseThrow(()->new NotFoundException("Utente non trovato" +
+                "con id" + " " + id));
+        utenteRepository.delete(utenteDaEliminare);
     }
 
 
