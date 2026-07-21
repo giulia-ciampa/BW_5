b08c7f04692c41f46190b0bd94dcd206fbf5055b
Andrea Saderi <andrea.saderi16@gmail.com>
2026-07-21 12:22:50 +0200

    updates indirizzo repository and service

--- src/main/java/team6/BW_5/repositories/IndirizzoRepository.java
+++ src/main/java/team6/BW_5/repositories/IndirizzoRepository.java
@@ -1,11 +1,27 @@
 package team6.BW_5.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import team6.BW_5.entities.Comune;
 import team6.BW_5.entities.Indirizzo;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface IndirizzoRepository extends JpaRepository<Indirizzo, UUID> {
+    @Query("SELECT i FROM Indirizzo i " +
+            "WHERE LOWER(i.via) = LOWER(:via) " +
+            "AND LOWER(i.civico) = LOWER(:civico) " +
+            "AND (:localita IS NULL OR LOWER(i.localita) = LOWER(:localita)) " +
+            "AND LOWER(i.cap) = LOWER(:cap) " +
+            "AND i.comune = :comune")
+    Optional<Indirizzo> findByViaCivicoLocalitaOptionalAndComune(
+            @Param("via") String via,
+            @Param("civico") String civico,
+            @Param("localita") String localita,
+            @Param("cap") String cap,
+            @Param("comune") Comune comune);
 }
--- src/main/java/team6/BW_5/services/IndirizzoService.java
+++ src/main/java/team6/BW_5/services/IndirizzoService.java
@@ -1,6 +1,8 @@
 package team6.BW_5.services;
 
 import org.springframework.stereotype.Service;
+import team6.BW_5.entities.Comune;
+import team6.BW_5.entities.Indirizzo;
 import team6.BW_5.repositories.IndirizzoRepository;
 
 @Service
@@ -11,5 +13,10 @@ public class IndirizzoService {
 
     public IndirizzoService(IndirizzoRepository indirizzoRepository) {
         this.indirizzoRepository = indirizzoRepository;
+    }
+
+
+    public Indirizzo findByViaCivicoLocalitaOptionalAndComune(String via, String civico, String localita, String cap, Comune comune) {
+        return indirizzoRepository.findByViaCivicoLocalitaOptionalAndComune(via, civico, localita, cap, comune).orElseGet(() -> indirizzoRepository.save(new Indirizzo(via, civico, localita, cap, comune)));
     }
 }
