cca2c4daaf47ff05f2d7bb25e43b7e213e16f58e
Andrea Saderi <andrea.saderi16@gmail.com>
2026-07-21 15:37:07 +0200

    Update Utente.java

--- src/main/java/team6/BW_5/entities/Utente.java
+++ src/main/java/team6/BW_5/entities/Utente.java
@@ -9,8 +9,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
@@ -50,6 +52,12 @@ public class Utente implements UserDetails {
 
     private String avatar;
 
+    @OneToMany(
+            mappedBy = "utente",
+            fetch = FetchType.LAZY
+    )
+    private List<AssegnazioneRuolo> assegnazioniRuolo = new ArrayList<>();
+
     public Utente(String username, String email, String password, String nome, String cognome) {
         this.username = username;
         this.email = email;
@@ -61,6 +69,11 @@ public class Utente implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return assegnazioniRuolo.stream()
+                .filter(assegnazione -> assegnazione.getDataRevoca() == null)
+                .map(AssegnazioneRuolo::getRuolo)
+                .map(RuoloUtente::getNomeRuolo)
+                .map(SimpleGrantedAuthority::new)
+                .toList();
     }
 }
