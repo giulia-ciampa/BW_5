b2d82ab7c7a8580ad269dceee5c45e974904dd67
Andrea Saderi <andrea.saderi16@gmail.com>
2026-07-23 11:19:58 +0200

    Create FileNotSupportedException.java

---
+++ src/main/java/team6/BW_5/exceptions/FileNotSupportedException.java
@@ -0,0 +1,7 @@
+package team6.BW_5.exceptions;
+
+public class FileNotSupportedException extends RuntimeException {
+    public FileNotSupportedException(String message) {
+        super(message);
+    }
+}
