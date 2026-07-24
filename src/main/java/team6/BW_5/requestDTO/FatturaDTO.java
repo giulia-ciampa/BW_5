b577db7518f694e85e831a4e74fa4b5b91d7c54f
giulia-ciampa <giulia.ciampa7@gmail.com>
2026-07-24 14:00:07 +0200

    Update FatturaDTO.java

--- src/main/java/team6/BW_5/requestDTO/FatturaDTO.java
+++ src/main/java/team6/BW_5/requestDTO/FatturaDTO.java
@@ -1,6 +1,5 @@
 package team6.BW_5.requestDTO;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
@@ -9,7 +8,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public record FatturaDTO(
-        @JsonFormat(pattern = "dd/MM/yyyy")
+
         @NotNull(message = "la data è obbligatoria")
         @PastOrPresent(message = "La data della fattura non può essere nel futuro")
         LocalDate data,
